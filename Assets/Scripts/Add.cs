4eff0a21af853b7b31fea60e839c2fefb90bf0e7
Jai Vanderark <1027859@student.sae.edu.au>
2025-07-06 19:01:20 +1000

    added summary to 2 scripts...

--- Assets/Scripts/Add.cs
+++ Assets/Scripts/Add.cs
@@ -17,63 +17,79 @@ public class Add : NetworkBehaviour, Health
 
     public GameObject attackSurface;
     public GameObject attackPos;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // On spawn in server
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
+        //find all players
         players = FindObjectsByType<Player>(FindObjectsSortMode.InstanceID);
+        //find the mime core
         Transform mimeCore = GameObject.FindWithTag("Finish").GetComponent<Transform>();
+        //set the default goal to be the mime core
         goal = mimeCore;
+        //50% chance
         if (Random.Range(0, 2) == 1)
         {
-            //go after players
+            //set goal to be randomly one of the players
             goal = players[Random.Range(0, players.Length)].transform;
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //if goal isnt null
         if (goal != null)
         {
+            //move towards the gaol based on speed
             transform.position = Vector3.MoveTowards(transform.position, goal.position, speed * Time.deltaTime);
         }
 
+        //idk...
         transform.position = new Vector3(transform.position.x, 1.286f, transform.position.z);
         
-        
+        //increment timer
         timer += Time.deltaTime;
+        //if timer is greater than attack time
         if (timer >= attackTime)
         {
-            //attack
+            //trigger attack function
             Attack();
+            //set the timer to 0
             timer = 0;
         }
     }
-
-    //TODO attack players and Mime Core
     
+    //Damage function
     public void Damage(int damage, ulong owner)
     {
+        //if is the Server and the attacker isn't ID 999
         if (IsServer && owner != 999)
         {
+            //reduce health by damage taken
             health-=damage;
 
+            //if health is less than 1
             if (health <= 0)
             {
                 ulong ownerIndex = owner;
                 int index = (int)ownerIndex;
-                    
+                
+                //increment the score of the killing player
                 FindFirstObjectByType<Eye>().playerScores[index] ++;
+                //despawn the object
                 GetComponent<NetworkObject>().Despawn();
+                //destroy the object
                 Destroy(gameObject);
             }
         }
     }
 
+    //attack function
     public void Attack()
     {
+        //spawn an attack surface
         GameObject ase = Instantiate(attackSurface, attackPos.transform.position, Quaternion.identity);
+        //spawn the attack surface on the network
         ase.GetComponent<NetworkObject>().Spawn();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,111 +21,109 @@ public class Player : NetworkBehaviour, Health
     public TextMeshPro titleText;
 
     public Slider hpSlider;
-    
-    //list of in network object id's of bullets, when it wants kill, cross from list for all
-    //public NetworkVariable<string> names;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
 
+    //on player spawning in the server
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
+        //is is the local player
         if (IsLocalPlayer)
         {
+            //find the hp slider
             hpSlider = FindFirstObjectByType<Slider>(FindObjectsInactive.Include);
+            //set it to active
             hpSlider.gameObject.SetActive(true);
             
+            //set the tag to Player
             gameObject.tag = "Player";
+            //set username
             string nme = "Player " + NetworkObjectId;
-            print(nme);
-/*
-            if (!names.Value.Contains(nme))
-            {
-                
-            }
-*/
-            //names.Value[NetworkObjectId]=nme;
             
+            //set player camera enabled
             cam.SetActive(true);
+            //lock the cursor to the centre
             Cursor.lockState = CursorLockMode.Locked;
+            //make the cursor not visible
             Cursor.visible = false;
             
-            //RequestSetName_Rpc("Player" + NetworkObjectId);
-            
+            //set the hp sliders max value to current health
             hpSlider.maxValue = health;
         }
         
+        //create array of all players
         Player[] players = FindObjectsOfType<Player>();
 
+        //for each player in the array
         for (int i = 0; i < players.Length; i++)
         {
+            //set their title text to their username
             players[i].titleText.text = "Player "+players[i].gameObject.GetComponent<NetworkObject>().NetworkObjectId;
         }
     }
 
     public void Update()
     {
+        //if is the local player
         if (IsLocalPlayer)
         {
+            //move the player based on Vertical and Horizontal axis inputs
             transform.position += transform.forward * Time.deltaTime * speed * Input.GetAxis("Vertical") +
                                   transform.right * Time.deltaTime * speed * Input.GetAxis("Horizontal");
             
+            //rotate the players X axis by their X mouse movement
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * Time.deltaTime * rotSpeed, 0);
             
+            //set the hp slider's value to the players health
             hpSlider.value = health;
         }
     }
 
-    [Rpc(SendTo.Server, RequireOwnership = false)]
-    void Rotate_Request_Rpc(float rot = 0)
-    {
-        Rotate_ServerResponse_Rpc(rot);
-    }
-
-    [Rpc(SendTo.ClientsAndHost, RequireOwnership = false)]
-    void Rotate_ServerResponse_Rpc(float rot = 0)
-    {
-        transform.rotation *= Quaternion.Euler(0, rot * Time.deltaTime * rotSpeed, 0);
-    }
-    
-
+    //Take damage function send to server
     [Rpc(SendTo.Server, RequireOwnership = false, Delivery = RpcDelivery.Reliable)]
     void RequestTakeDamage_Rpc(int newHealth)
     {
         TakeDamage_Rpc(newHealth);
     }
 
+    //Take damage function on server
     [Rpc(SendTo.ClientsAndHost, RequireOwnership = false)]
     void TakeDamage_Rpc(int newHealth)
     {
+        //set the players health to the new health
         health = newHealth;
+        //if health is less than 1
         if (health <= 0)
         {
+            //send Respawn function trigger to server
             RequestRespawn_Rpc(5);
         }
     }
 
-
+    //send Respawn function trigger to server
     [Rpc(SendTo.Server, RequireOwnership = false, Delivery = RpcDelivery.Reliable)]
     void RequestRespawn_Rpc(int respawnHealth)
     {
         Respawn_Rpc(respawnHealth);
     }
 
+    //Respawn function on server
     [Rpc(SendTo.ClientsAndHost, RequireOwnership = false)]
     void Respawn_Rpc(int respawnHealth)
     {
+        //set the players position to 0,0,0 (world origin)
         transform.position = Vector3.zero;
+        //set the players health to respawn health (default health)
         health = respawnHealth;
+        //update the hp slider to reflect health
         hpSlider.value = health;
 
     }
 
+    //server damage function
     public void Damage(int damage, ulong owner)
     {
         ulong killerIndex = owner;
+        //send damage function trigger to server
         RequestTakeDamage_Rpc(health - damage);
     }
 }
